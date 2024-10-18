79d107a69a4e8444ef22f92ba958b53ba94d9f0f
Enzo Daniel Bertolusso <151404661+enzodb97@users.noreply.github.com>
2024-10-18 15:24:55 -0300

    agregamos componentes:
    agregamos componentes products-list

---
+++ Frontend/inventory-app/src/app/product.ts
@@ -0,0 +1,9 @@
+export class Product {
+    idProducts: number;
+    name: string;
+    description:string ;
+    price: number;
+    stockQuantity: number;
+    category: string;
+    createdAt: Date;
+}
